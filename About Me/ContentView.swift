a914d2da3afe5a6e7dc147bebf82de831b9e1740
Samantha Castillo <smcastillo22@amarillocollege.com>
2023-11-01 16:42:57 +0000

    add deadline

--- About Me/ContentView.swift
+++ About Me/ContentView.swift
@@ -2,18 +2,39 @@
 //  ContentView.swift
 //  About Me
 //
-//  Created by [YOUR NAME GOES HERE].
+//  Created by Samantha Castillo.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = "About Me"
+    @State private var image = "person.fill.questionmark.rtl"
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName:image)
+                .scaledToFill()
+                .font(.title)
+                .foregroundColor(.mint)
+            Text(message)
+                .font(.subheadline)
+                .fontWeight(.semibold)
+            HStack {
+                Button("Hobby") {
+                    message = "I make art as a hobby."
+                    image = "paintbrush.pointed.fill"
+                }
+                Button("Music") {
+                    message = "My favorite music is Indie music."
+                    image = "music.note"
+                }
+                Button("Dream") {
+                    message = "I'd like to make my own comic someday."
+                    image = "exclamationmark.bubble"
+                }
+            }
+            .buttonStyle(.borderedProminent)
         }
         .padding()
     }
